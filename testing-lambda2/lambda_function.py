8867d8e84782676d4f677e7f033c95ce46b91919
sebastian-spitta-nuvu <sebastian.spitta@nuvu.cc>
2025-04-10 08:59:23 -0500

    testing workflow 2

--- testing-lambda2/lambda_function.py
+++ testing-lambda2/lambda_function.py
@@ -1,6 +1,6 @@
 def prime_number(x):
     for n in range(2,x):
         if x%n == 0:
-            print("HOla mundo!!!")
+            print("HOla mundo!! :D!")
             return False
     return True
