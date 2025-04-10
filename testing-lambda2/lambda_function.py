fd6e0a6beae4c7733d8bf0b39d73b4623dc6e605
sebastian-spitta-nuvu <sebastian.spitta@nuvu.cc>
2025-04-10 09:22:59 -0500

    update lambda 2

--- testing-lambda2/lambda_function.py
+++ testing-lambda2/lambda_function.py
@@ -1,6 +1,6 @@
 def prime_number(x):
     for n in range(2,x):
         if x%n == 0:
-            print("HOla mundo!! :D!")
+            print("HOla mundo!! :D! ahora voy a hacer un push aqui")
             return False
     return True
